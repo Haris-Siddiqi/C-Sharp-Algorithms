7113d181989e59d7677352ea4caa263d209275c2
Haris-Siddiqi <45279509+Haris-Siddiqi@users.noreply.github.com>
2020-03-03 14:32:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var
+            double gpa;
+            double score;
+
+            // input
+            Console.WriteLine("Please enter your GPA:");
+            gpa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your admission score:");
+            score = Convert.ToDouble(Console.ReadLine());
+
+            // if statement
+            if (gpa >= 3.0)
+            {
+                if (score >= 60)
+                {
+                    Console.WriteLine("Accept");
+                }
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Accept");
+            }
+            else
+            {
+                Console.WriteLine("Reject");
+            }
+        }
+    }
+}
