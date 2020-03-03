9f94db561fe7d90bf1c2028b0e44bc89a3cd94a7
Haris-Siddiqi <45279509+Haris-Siddiqi@users.noreply.github.com>
2020-03-03 14:37:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var
+            Int32 randomNumber;
+            Int32 guess;
+            string message = "";
+
+            // random number generator
+            Random ranNumberGenerator = new Random();
+            randomNumber = ranNumberGenerator.Next(1, 11);
+
+            // input
+            Console.WriteLine("I'm thinking of a number between 1 and 10...");
+            Console.Write("Take a guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            // if statement
+            if (guess == randomNumber)
+            {
+                message = "correct!";
+            }
+            else
+            {
+                if (guess < randomNumber)
+                {
+                    message = "too low...";
+                }
+                else
+                {
+                    message = "too high...";
+                }
+            }
+
+            // output
+            Console.WriteLine("\nMy number was " + randomNumber + "...");
+            Console.WriteLine("Your guess was " + message);
+        }
+    }
+}
