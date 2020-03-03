d6ba7d4151769d02a463977ef1221c7238ba2db8
Haris-Siddiqi <45279509+Haris-Siddiqi@users.noreply.github.com>
2020-03-03 14:34:35 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,87 @@
 ﻿using System;
 
-namespace Ex11
+namespace Ex18
 {
     class Program
     {
         static void Main(string[] args)
         {
             // var
-            double gpa;
-            double score;
+            Int32 num1;
+            Int32 num2;
+            Int32 num3;
+            string num;
+            Int32 guess1;
+            Int32 guess2;
+            Int32 guess3;
+            string guess;
+            Int32 matches = 0;
+            Int32 award;
+            bool order = false;
+
+            // generate ran num
+            Random ranNumberGenerator = new Random();
+            num1 = ranNumberGenerator.Next(1, 5);
+            num2 = ranNumberGenerator.Next(1, 5);
+            num3 = ranNumberGenerator.Next(1, 5);
 
             // input
-            Console.WriteLine("Please enter your GPA:");
-            gpa = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter your admission score:");
-            score = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your first guess:");
+            guess1 = Convert.ToInt32(Console.ReadLine());
 
-            // if statement
-            if (gpa >= 3.0)
+            Console.WriteLine("Enter your second guess:");
+            guess2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your third guess:");
+            guess3 = Convert.ToInt32(Console.ReadLine());
+
+            // amalgamate numbers
+            num = Convert.ToString(num1) + Convert.ToString(num2) + Convert.ToString(num3);
+            guess = Convert.ToString(guess1) + Convert.ToString(guess2) + Convert.ToString(guess3);
+
+            // guessed all three numbers in correct order
+            if (num == guess)
             {
-                if (score >= 60)
-                {
-                    Console.WriteLine("Accept");
-                }
-            }
-            else if (score >= 80)
-            {
-                Console.WriteLine("Accept");
+                order = true;
             }
             else
             {
-                Console.WriteLine("Reject");
+                // compare each guess to each num
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int k = 0; i < 3; k++)
+                    {
+                        if (guess[i] == num[k])
+                        {
+                            matches += 1;
+                        }
+                    }
+                }
             }
+
+            // matches
+            if (order == true)
+            {
+                award = 10000;
+            }
+            else
+            {
+                if (matches == 1)
+                {
+                    award = 10;
+                }
+                else if(matches == 2)
+                {
+                    award = 100;
+                }
+                else
+                {
+                    award = 1000;
+                }
+            }
+
+            // output
+            Console.WriteLine($"Your award is {award, 0:C}.");
         }
     }
 }
